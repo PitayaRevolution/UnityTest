294e0e3a2d096a12b8d97475b497b4a474532021
Nymeria <zhoulw063922@gmail.com>
2020-08-24 21:41:30 +0800

    init commit

---
+++ Assets/Scripts/EventTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventTrigger : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Down.."+eventData.ToString());
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("112358");
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    PlayerControl pc;
+
+    public float speed = 5f;
+    float move;
+    bool jump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pc = GetComponent<PlayerControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+        jump = Input.GetButton("Jump");
+    }
+
+    private void FixedUpdate()
+    {
+        move *= speed;
+        pc.Move(move, jump);
+    }
+}
